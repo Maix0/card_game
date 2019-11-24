483a747e52be467fb7c763d8df27c6c8e5324e6f
maix0 <maix522@gmail.com>
2019-11-24 19:17:56 +0000

    added some example for death event

--- deck_generator.py
+++ deck_generator.py
@@ -1,30 +1,40 @@
 import json
 
 # Carte 1 :
-tank = {"name":"Tank","atk":1,"vie":1,"cout":1,"path":"path_","qte":4, "taunt":False}
+tank = {"name":"Tank","atk":1,"vie":1,"cout":1,"path":"placeholder","qte":4, "taunt":False}
 # Carte 2 :
-panda_roux = {"name":"Panda roux","atk":1,"vie":2,"cout":2,"path":"path_","qte":3,"taunt":False}
+panda_roux = {"name":"Panda roux","atk":1,"vie":2,"cout":2,"path":"placeholder",
+              "qte":3,"taunt":False}
 # Carte 3 :
 canard_en_plastique = {"name":"Canard en \
-plastique","atk":3,"vie":2,"cout":3,"path":"path_","qte":1, "taunt":True}
+plastique","atk":3,"vie":2,"cout":3,"path":"placeholder","qte":1, "taunt":True}
 # Carte 4 :
-palpatine = {"name":"Palpatine","atk":4,"vie":2,"cout":4,"path":"path_","qte":1, "taunt":False}
+palpatine = {"name":"Palpatine","atk":4,"vie":2,"cout":4,
+             "path":"placeholder","qte":1, "taunt":False}
 # Carte 5 :
-terminator = {"name":"Terminator","atk":2,"vie":3,"cout":3,"path":"path_","qte":1,"taunt":False}
+terminator = {"name":"Terminator","atk":2,"vie":3,"cout":3,
+              "path":"placeholder","qte":1,"taunt":False}
 # Carte 6 :
-sid = {"name":"Sid","atk":3,"vie":3,"cout":3,"path":"path_","qte":1,"taunt":False}
+sid = {"name":"Sid","atk":3,"vie":3,"cout":3,
+       "path":"placeholder","qte":1,"taunt":False}
 # Carte 7 :
-patchouli = {"name":"Patchouli","atk":2,"vie":1,"cout":1,"path":"path_","qte":3,"taunt":False}
+patchouli = {"name":"Patchouli","atk":2,"vie":1,"cout":1,
+             "path":"./patchouli.gif","qte":3,"taunt":False}
 # Carte 8 :
-moussa = {"name":"Moussa","atk":2,"vie":3,"cout":3,"path":"path_","qte":1,"taunt":True}
+moussa = {"name":"Moussa","atk":2,"vie":3,"cout":3,
+          "path":"placeholder","qte":1,"taunt":True}
 # Carte 9 :
-projecteur = {"name":"Projecteur Epson","atk":1,"vie":1,"cout":10,"path":"path_","qte":1,"taunt":True}
+projecteur = {"name":"Projecteur Epson","atk":1,"vie":1,"cout":10,
+              "path":"placeholder","qte":1,"taunt":True}
 # Carte 10 :
-quenouille = {"name":"Quenouille","atk":2,"vie":2,"cout":2,"path":"path_","qte":2,"taunt":False}
+quenouille = {"name":"Quenouille","atk":2,"vie":2,"cout":2,
+              "path":"placeholder","qte":2,"taunt":False}
 # Carte 11 :
-devoir_maison = {"name":"Devoir Maison","atk":7,"vie":5,"cout":7,"path":"path_","qte":1,"taunt":True}
+devoir_maison = {"name":"Devoir Maison","atk":7,"vie":5,"cout":7,
+                 "path":"placeholder","qte":1,"taunt":True}
 # Carte 12 :
-ronald = {"name":"Ronald McDonald","atk":9,"vie":1,"cout":5,"path":"path_","qte":1,"taunt":False}
+ronald = {"name":"Ronald McDonald","atk":9,"vie":1,"cout":5,
+          "path":"placeholder","qte":1,"taunt":False}
 
 """
     END OF CARDS DEF
@@ -47,7 +57,8 @@ for definition in CARDS:
     card = {}
     for key in definition:
         if key in FORMAT:
-            card[FORMAT[key]] = definition[key] if key != "path" else "patchouli.gif"
+            card[FORMAT[key]] = definition[key] if definition[key] != "placeholder"\
+                                                else "patchouli.gif"
         else:
             if key not in FORMAT_BL:
                 print("KEY `{}` not in FORMAT && FORMAT_BL for card : {}".format(key,card))
@@ -55,3 +66,4 @@ for definition in CARDS:
 
 f = open(OUT_FILE,"w")
 json.dump(deck,f)
+f.close()
